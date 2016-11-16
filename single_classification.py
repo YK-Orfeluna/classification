38584e0fae04f94eec4bc0eced10e3f57f75632b
Yuki Kitagishi <k030913@kansai-u.ac.jp>
2016-11-16 19:04:01 +0900

    161115

---
+++ single_classification.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+import sys 
+import numpy as np
+from sklearn import datasets, neighbors, svm
+from sklearn.model_selection import GridSearchCV
+
+argvs = sys.argv
+if len(argvs) == 2 :
+	filename = argvs[1] + ".csv"
+	print(filename)
+	data = np.genfromtxt(filename, delimiter=',')
+	col, row = data.shape
+	X_learn = data[:,:row-1]
+	y_learn = data[:,row-1:]
+else :
+	print("To Use 'Iris-Data'")
+	X_learn = datasets.load_iris().data
+	y_learn = datasets.load_iris().target
+
+c = np.append(np.array([1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
+gamma = np.append(np.array([0, 1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
+degree = np.arange(2, 11)
+neighbor = np.arange(1, 16)
+weight = ["uniform", "distance"]
+
+#c = gamma = degree = neighbor = [1, 2, 3]
+
+cv = 20
+jobs = 2
+
+class App() :
+
+	def __init__(self) :
+		self.label = "Classification_"
+
+	def output(self, t, p) :
+		t.write(str(p) + "\n")
+		print(p)
+
+	def main(self, classification) :
+		label = "Classification_"
+
+		if classification == "knn" :
+			est = neighbors.KNeighborsClassifier()
+			param = {"n_neighbors":neighbor ,"weights":weight}
+		elif classification == "liner" :
+			est = svm.SVC()
+			param = {"kernel":["linear"], "C":c}
+		elif classification == "rbf" :
+			est = svm.SVC()
+			param = {"kernel":["rbf"], "C":c, "gamma":gamma}
+		elif classification == "poly" :
+			est = svm.SVC()
+			param = {"kernel":["poly"], "C":c, "degree":degree}
+		else :
+			sys.exit("Only 'knn', 'liner', 'rbf'")
+
+		if classification == "knn" :
+			label += classification
+		else :
+			label += "SVM_%s" %classification
+		
+		t = open(label + ".txt", "w")
+		self.output(t, label)
+
+
+		clf = GridSearchCV(est, param, cv=cv, n_jobs=jobs)
+		clf.fit(X_learn, y_learn)
+		print("fit")
+
+		b_score = "Best Score: %s" %clf.best_score_
+		self.output(t, b_score)
+		b_param = "Best Param: %s" %clf.best_params_
+		self.output(t, b_param)
+
+		di = clf.cv_results_
+		di_params = "Each-Param: %s" %str(di["params"])
+		self.output(t, di_params)
+		mean = "Each Mean-Score: %s" %di["mean_test_score"]
+		self.output(t, mean)
+		std = "Each Std-Score: %s" %di["std_test_score"]
+		self.output(t, std)
+
+		"""
+		for k, v in sorted(di.items()):
+			print(str(k) + ": " + str(v) + "\n")
+		"""
+
+		t.close()
+
+if __name__ == "__main__" :
+	app = App()
+	app.main("knn")
+	app.main("liner")
+	app.main("poly")
+	app.main("rbf")
+
+	sys.exit()
