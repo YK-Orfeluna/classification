2943d07fb70187b652578375c7212cd652319029
Yuki Kitagishi <mokona965@gmail.com>
2017-01-02 01:05:00 +0900

    170101

--- classification.py
+++ classification.py
@@ -9,29 +9,37 @@ CSV = ","
 TSV = "\t"
 DEL = CSV
 
-TRAIN = ""
-#TRAIN = np.genfromtxt(TRAIN, delimiter=DEL)
+TRAIN = "iris_train.csv"
+TRAIN = np.genfromtxt(TRAIN, delimiter=DEL)
 
-TEST = ""
-#TEST = np.genfromtxt(TEST, delimiter=DEL)
+TEST = "iris_test.csv"
+TEST = np.genfromtxt(TEST, delimiter=DEL)
 
-FLAG = "f"
-#FLAG = "cross"
+FLAG = "f"					# 学習データとテストデータでF値を使って精度検証
+#FLAG = "cross"				# K分割交差検定を行う（K = CV）
 
-IRIS = True
-#IRIS = False
+#IRIS = True				# irisデータを使ったデモ
+IRIS = False
 
-JOBS = 2
+JOBS = 2					# 同時進行スレッド数
 
-C = np.append(np.array([1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
-GAMMA = np.append(np.array([0, 1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
-DEGREE = np.arange(2, 11)
-NEIGHBOR = np.arange(1, 16)
+if IRIS :					# irisｓデータを扱う場合のチューニング変数
+	C = GAMMA = DEGREE = NEIGHBOR = [1, 2, 3, 4, 5]
+else :						# 通常のチューニング変数
+	C = np.append(np.array([1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
+	GAMMA = np.append(np.array([0, 1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
+	DEGREE = np.arange(2, 11)
+	NEIGHBOR = np.arange(1, 16)
 WEIGHT = ["uniform", "distance"]
 CV = 20
 
-if FLAG == "f" :
-	if IRIS :
+PARAM_KNN = {"n_neighbors":NEIGHBOR ,"weights":WEIGHT}
+PARAM_LINER = {"kernel":["linear"], "C":C}
+PARAM_POLY = {"kernel":["poly"], "C":C, "degree":DEGREE}
+PARAM_RBF = {"kernel":["rbf"], "C":C, "gamma":GAMMA}
+
+if FLAG == "f" :			# F値で検定を行う場合の，学習・テストデータの作成
+	if IRIS :				# irisデータを扱う場合
 		iris = datasets.load_iris()
 		x_train = iris.data[xrange(0, len(iris.data), 2)]
 		y_train = iris.target[xrange(0, len(iris.data), 2)]
@@ -47,8 +55,8 @@ if FLAG == "f" :
 		x_test = TEST[:,:row2-1]
 		y_test = TEST[:,row2-1:]
 
-elif FLAG == "cross" :
-	if IRIS :
+elif FLAG == "cross" :		# 交差検定を行う場合の，学習データの作成
+	if IRIS :				# irisデータを扱う場合
 		x_train = datasets.load_iris().data
 		y_learn = datasets.load_iris().target
 
@@ -56,35 +64,6 @@ elif FLAG == "cross" :
 		row1 = TRAIN.shape[1]
 		x_train = data[:,:row1-1]
 		y_train = data[:,row1-1:]
-
-argvs = sys.argv								# コマンドライン引数を取得する
-if len(argvs) == 3 :							# 引数の数が適切な時（3）
-	filename = argvs[1] + ".csv"				# 1番目の引数をcsvファイルの名前として取り扱う（ただし".csv"は入力不要）
-	print(filename)
-	data = np.genfromtxt(filename, delimiter=',')
-	col, row = data.shape
-	X_learn = data[:,:row-1]
-	y_learn = data[:,row-1:]
-
-	# チューニング用の変数を作る
-	c = np.append(np.array([1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
-	gamma = np.append(np.array([0, 1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
-	degree = np.arange(2, 11)
-	neighbor = np.arange(1, 16)
-
-	jobs = int(argvs[2])						# 2番目の引数を並行処理の数として取り扱う（コア数/スレッド数を超えても無駄）
-
-else :											# コマンドライン引数が適切に入力されなかった場合，irisデータを使ったテストを行う
-	print("To Use 'Iris-Data'")
-	X_learn = datasets.load_iris().data
-	y_learn = datasets.load_iris().target
-
-	c = gamma = degree = neighbor = [1, 2, 3]	# チューニング用の変数を作る
-	jobs = 2									# 並行処理の同時並行数
-
-weight = ["uniform", "distance"]				# knnの変数
-cv = 20											# グリッドサーチの回数
-
 
 class App() :
 	def __init__(self) :
