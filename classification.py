1f7ebcf63886483cbf649bc8389005fd9893fad4
Yuki Kitagishi <k030913@kansai-u.ac.jp>
2017-01-06 15:37:58 +0900

    160106

--- classification.py
+++ classification.py
@@ -5,60 +5,109 @@ import numpy as np
 from sklearn import datasets, neighbors, svm
 from sklearn.model_selection import GridSearchCV
 
-argvs = sys.argv								# コマンドライン引数を取得する
-if len(argvs) == 3 :							# 引数の数が適切な時（3）
-	filename = argvs[1] + ".csv"				# 1番目の引数をcsvファイルの名前として取り扱う（ただし".csv"は入力不要）
-	print(filename)
-	data = np.genfromtxt(filename, delimiter=',')
-	col, row = data.shape
-	X_learn = data[:,:row-1]
-	y_learn = data[:,row-1:]
+#FLAG = "f"					# 学習データとテストデータでF値を使って精度検証
+FLAG = "cross"				# K分割交差検定を行う（K = CV）
 
-	# チューニング用の変数を作る
-	c = np.append(np.array([1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
-	gamma = np.append(np.array([0, 1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
-	degree = np.arange(2, 11)
-	neighbor = np.arange(1, 16)
+KNN = True
+#KNN = False
 
-	jobs = int(argvs[2])						# 2番目の引数を並行処理の数として取り扱う（コア数/スレッド数を超えても無駄）
+LINER = True
+#LINER = False
 
-else :											# コマンドライン引数が適切に入力されなかった場合，irisデータを使ったテストを行う
-	print("To Use 'Iris-Data'")
-	X_learn = datasets.load_iris().data
-	y_learn = datasets.load_iris().target
+POLY = True
+#POLY = False
 
-	c = gamma = degree = neighbor = [1, 2, 3]	# チューニング用の変数を作る
-	jobs = 2									# 並行処理の同時並行数
+RBF = True
+#RBF = False
 
-weight = ["uniform", "distance"]				# knnの変数
-cv = 20											# グリッドサーチの回数
+#IRIS = True				# irisデータを使ったデモ
+IRIS = False
 
+CSV = ","
+TSV = "\t"
+DEL = CSV
+
+TRAIN = "iris_train.csv"
+TRAIN = np.genfromtxt(TRAIN, delimiter=DEL)
+
+TEST = "iris_test.csv"
+TEST = np.genfromtxt(TEST, delimiter=DEL)
+
+if IRIS :					# irisデータを扱う場合のチューニング変数
+	C = GAMMA = DEGREE = NEIGHBOR = [1, 2, 3, 4, 5]
+else :						# 通常のチューニング変数
+	C = np.append(np.array([1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
+	GAMMA = np.append(np.array([0, 1e-06, 1e-05, 1e-04, 0.001, 0.01]), np.arange(0.1, 30.1, 0.1))
+	DEGREE = np.arange(2, 11)
+	NEIGHBOR = np.arange(1, 16)
+WEIGHT = ["uniform", "distance"]
+CV = 20
+JOBS = 2					# 同時進行スレッド数
+
+PARAM_KNN = {"n_neighbors":NEIGHBOR ,"weights":WEIGHT}
+PARAM_LINER = {"kernel":["linear"], "C":C}
+PARAM_POLY = {"kernel":["poly"], "C":C, "degree":DEGREE}
+PARAM_RBF = {"kernel":["rbf"], "C":C, "gamma":GAMMA}
 
 class App() :
 	def __init__(self) :
 		self.label = "Classification_"
 
+	def data(self) :				# データ作成
+		if FLAG == "f" :			# F値で検定を行う場合の，学習・テストデータの作成
+			if IRIS :				# irisデータを扱う場合
+				iris = datasets.load_iris()
+
+				self.x_train = iris.data[xrange(0, len(iris.data), 2)]
+				self.y_train = iris.target[xrange(0, len(iris.data), 2)]
+
+				self.x_test = iris.data[xrange(1, len(iris.data), 2)]
+				self.y_test = iris.target[xrange(1, len(iris.data), 2)]
+
+				target_names = iris.target_names
+
+			else :
+				row = TRAIN.shape[1]
+				self.x_train = TRAIN[:,:row-1]
+				self.y_train = TRAIN[:,row-1:]
+
+				row = TEST.shape[1]
+				self.x_test = TEST[:,:row-1]
+				self.y_test = TEST[:,row-1:]
+
+		elif FLAG == "cross" :		# 交差検定を行う場合の，学習データの作成
+			if IRIS :				# irisデータを扱う場合
+				self.x_train = datasets.load_iris().data
+				self.y_train = datasets.load_iris().target
+
+			else :
+				row1 = TRAIN.shape[1]
+				self.x_train = TRAIN[:,:row1-1]
+				self.y_train = TRAIN[:,row1-1:]
+				if len(y_train.shape) == 2 :
+					self.y_train = self.y_train[:, 0]
+
 	def output(self, t, p) :
 		t.write(str(p) + "\n")
 		print(p)
 
-	def main(self, flag) :
+	def cross(self, flag) :
 		label = self.label
 
 		if flag == "knn" :
 			est = neighbors.KNeighborsClassifier()
-			param = {"n_neighbors":neighbor ,"weights":weight}
+			param = PARAM_KNN
 		elif flag == "liner" :
 			est = svm.SVC()
-			param = {"kernel":["linear"], "C":c}
+			param = PARAM_LINER
 		elif flag == "rbf" :
 			est = svm.SVC()
-			param = {"kernel":["rbf"], "C":c, "gamma":gamma}
+			param = PARAM_RBF
 		elif flag == "poly" :
 			est = svm.SVC()
-			param = {"kernel":["poly"], "C":c, "degree":degree}
+			param = PARAM_POLY
 		else :
-			sys.exit("Only 'knn', 'liner', 'rbf'")
+			sys.exit("Only 'knn', 'liner', 'poly' or rbf'")
 
 		if flag == "knn" :
 			label += flag
@@ -69,8 +118,8 @@ class App() :
 		self.output(t, label)
 
 
-		clf = GridSearchCV(est, param, cv=cv, n_jobs=jobs)
-		clf.fit(X_learn, y_learn)
+		clf = GridSearchCV(est, param, cv=CV, n_jobs=JOBS)
+		clf.fit(x_train, y_train)
 		print("fit")
 
 		b_score = "Best Score: %s" %clf.best_score_
@@ -95,9 +144,13 @@ class App() :
 
 if __name__ == "__main__" :
 	app = App()
-	app.main("knn")
-	app.main("liner")
-	app.main("poly")
-	app.main("rbf")
-
-	sys.exit("System Exit")
+	
+	if FLAG == "cross" :
+		if KNN :
+			app.cross("knn")
+		if LINER :
+			app.cross("liner")
+		if POLY :
+			app.cross("poly")
+		if RBF :
+			app.cross("rbf")
