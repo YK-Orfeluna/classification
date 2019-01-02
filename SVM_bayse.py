ccdb6d2da22facb143ba5703ab5fe7afd4c33da9
YK-Orfeluna <k030913@kansai-u.ac.jp>
2019-01-02 18:22:20 +0900

    add loss function

--- SVM_bayse.py
+++ SVM_bayse.py
@@ -5,9 +5,9 @@ try :
 except ImportError :
 	exit("You have to install \"scikit-optimize.\"")
 
-
+import numpy as np
 from sklearn import datasets, svm
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, train_test_split
 
 iris = datasets.load_iris()
 
@@ -19,6 +19,7 @@ params = [	[2**-30, 2**30, L],
 			]
 
 def f(x) :
+	global clf
 	#C, gamma, kernel, degree = x
 	C, gamma = x
 	svc = svm.SVC(C=C, gamma=gamma)
@@ -27,6 +28,54 @@ def f(x) :
 
 	return clf.best_score_ * -1
 
-res = gp_minimize(f, params, acq_func="EI", n_calls=100, n_jobs=6)
-print("Best Parameters\n\tC: %s\n\tgamma: %s" %(res.x[0], res.x[1]))
-print("Best Score: %s" %res.fun*-1)
+def f1(x) :
+	global clf
+	C, gamma = x
+	clf = svm.SVC(C=C, gamma=gamma, probability=True)
+
+	clf.fit(X_train, y_train)
+	score = clf.score(X_test, y_test)
+	return score * -1
+
+def f2(x) :
+	global clf
+	C, gamma = x
+	clf = svm.SVC(C=C, gamma=gamma, probability=True)
+
+	clf.fit(X_train, y_train)
+
+	scores = clf.predict_proba(X_test)
+
+	answer = np.zeros_like(scores)
+	for x, i in enumerate(y_test) :
+		answer[x, i] = 1.0
+
+	#mean squared error
+	#error = 0.5 * np.sum((answer-scores)**2)
+
+	# cross entropy error
+	delta = 1e-7
+	error = -np.sum(answer*np.log(scores+delta))
+
+	return error
+
+if __name__ == "__main__" :
+	clf = svm.SVC()
+
+	N = 100
+	J = 8
+
+	X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.5)
+
+	res1 = gp_minimize(f1, params, acq_func="EI", n_calls=N, n_jobs=J)
+	res2 = gp_minimize(f2, params, acq_func="EI", n_calls=N, n_jobs=J)
+
+	print("Best Parameters\n\tC: %s\n\tgamma: %s" %(res1.x[0], res1.x[1]))
+	print("Best Score: %s" %(res1.fun*-1))
+
+	print("---------------------------------------------")
+
+	print("Best Parameters\n\tC: %s\n\tgamma: %s" %(res2.x[0], res2.x[1]))
+	print("Best Score: %s" %clf.score(X_test, y_test))
+	print("Minimum Error: %s" %res2.fun)
+
